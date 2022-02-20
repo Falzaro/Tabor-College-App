3b65c77cdb4727d04c9763d2b49ee33a6b06fedf
Fitri <fitrirozi1@gmail.com>
2022-02-20 16:44:23 -0600

    update

--- App.js
+++ App.js
@@ -1,58 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-=======
-import React from 'react';
-import {StyleSheet, View, Text, Image} from 'react-native';
-
-import MenuScreen from './screens/MenuScreen.js';
-
->>>>>>> 4e332832759686f7911fab62c5544be9e1b646d6
-
-export default function App() {
-    return (
-       <View style={styles.container}>
-          <MenuScreen title="Tabor College" />
-       </View>
-    );
-}
-
-const styles = StyleSheet.create({
-<<<<<<< HEAD
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-=======
-import { Linking } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-    const hyperLinks = [
-        ["Campus Cafe", "https://tab-web.scansoftware.com/cafeweb/login"],
-        ["Tabor Cafe Menu", "https://oncampusdining.com/tabor/menus/"],
-        ["Tabor Main Page", "https://tabor.edu/"],
-        ["Sports", "https://www.taborbluejays.com/"],
-        ["Calendar", "https://tabor.edu/calendar/"],
-        ["News", "https://tabor.edu/news"],
-        ["library", "https://taborcollege.libguides.com/library"],
-    ];
-
     return (
         <View style={styles.container}>
-            {hyperLinks.map(([name, url]) => (
-                <Text
-                    key={name}
-                    style={{ marginVertical: 18 }}
-                    onPress={() => Linking.openURL(url)}
-                >
-                    {name}
-                </Text>
-            ))}
+            <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
         </View>
     );
@@ -65,11 +17,4 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
     },
->>>>>>> parent of 4e33283... Comit for Front_End
 });
-=======
-    container: {
-        flex: 1,
-    }
-})
->>>>>>> 4e332832759686f7911fab62c5544be9e1b646d6
