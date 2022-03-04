435bc24ad08dcc039f9280ec0c8ef615458af570
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2022-03-03 18:38:56 -0600

    Update Icons

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,13 +1,22 @@
-import React, { Component} from 'react';
-import {View, Text, StyleSheet, ImageBackground, Image, TouchableHighlight, Alert, TouchableOpacity,} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { useState} from 'react';
+import {View, Text, StyleSheet, ImageBackground, Image, Alert, TouchableOpacity, Linking} from 'react-native';
+import ButtonLabel from '../components/SettingButtonLabel.js';
 
-import ButtonLabel from '../components/ButtonLabel.js';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-const MenuScreen = (props) => {
+
+import Foundation from 'react-native-vector-icons/Foundation';
+
+
+
+const MenuScreen = ({title}) => {
+
     return (
         <View style = {styles.screen} >
-
             {/* Setting up Image Header */}
         <View  style = {styles.coverImage} >
             
@@ -17,72 +26,80 @@ const MenuScreen = (props) => {
 
              {/* Setting up Text Header */}
              <View style = {styles.textView} >  
-                <Text style = {styles.imageText}>{props.title}</Text>
+                <Text style = {styles.imageText}>{title}</Text>
             </View>
             </ImageBackground>
         </View>
+        
+        <View style = {styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={() => Linking.openURL('https://tabor.instructure.com/login/ldap')}>
+                <Image style={styles.imageIcon} width="50%" height="50%" source = {require('../assets/Canvas-Logo.png')}/>
+                <Text style={styles.buttonText}>Canvas</Text>
+            </TouchableOpacity>
 
-        <View style = {styles.row} >
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <Ionicons name="people" size = {50} color="#003082" />
+                <Text style={styles.buttonText}>Student Life</Text>
+            </TouchableOpacity>
 
-            <View style = {styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <MaterialIcons name="local-dining" size={50} color="#003082" />
+                <Text style={styles.buttonText}>Dining</Text>
+            </TouchableOpacity>
 
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <Ionicons name="cafe" size={50} color="#003082" />
+                <Text style={styles.buttonText}>Campus Cafe</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
-                    <Icon name="home" size={40} color="#003082" />
-                    <Text style={styles.buttonText}>Canvas</Text>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <FontAwesome5 name="calendar" size={50} color="#003082" />
+                <Text style={styles.buttonText}>Calendar</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
-                    <Icon name='coffee' size={40} color="#003082" />
-                    <Text style={styles.buttonText}>Student Life</Text>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <MaterialCommunityIcons name="shopping-outline" size={50} color="#003082" />
+                <Text style={styles.buttonText}>Jayshop</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
-                    <Icon name='coffee' size={40} color="#003082" />
-                    <Text style={styles.buttonText}>Dining</Text>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <FontAwesome5 name="map-marked-alt" size={50} color="#003082" />
+                <Text style={styles.buttonText}>Maps</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
-                    <Icon name='coffee' size={40} color="#003082" />
-                    <Text style={styles.buttonText}>Campus Cafe</Text>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <Ionicons name="american-football" size={50} color="#003082" />
+                <Text style={styles.buttonText}>Sports</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
-                    <Icon name='coffee' size={40} color="#003082" />
-                    <Text style={styles.buttonText}>Calendar</Text>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <MaterialCommunityIcons name="account-clock-outline" size={50} color="#003082" />
+                <Text style={styles.buttonText}>Helpful Hours</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
-                    <Icon name='apple' size={40} color="#003082" />
-                    <Text style={styles.buttonText}>Jayshop</Text>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <FontAwesome name="newspaper-o" size={50} color="#003082" />
+                <Text style={styles.buttonText}>News</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
-                    <Icon name='map' size={40} color="#003082" />
-                    <Text style={styles.buttonText}>Maps</Text>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
+                <Ionicons name="notifications-outline" size={40} color="#003082" />
+                <Text style={styles.buttonText}>Notifications</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
-                    <Icon name='bell' size={40} color="#003082" />
-                    <Text style={styles.buttonText}>Notifications</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Testing')}>
-                    <Icon name='bell' size={40} color="#003082" />
-                    <Text style={styles.buttonText}>Notifications</Text>
-                </TouchableOpacity>
-
-
-            </View>
+            <TouchableOpacity style={styles.button} >
+                <Foundation name="credit-card" size={40} color="#003082" />
+                <Text style={styles.buttonText}>Notifications</Text>
+            </TouchableOpacity>
+        </View>
             {/*Setting Button and Customize Navigation Bar */}
-
-            <View>
-                <ButtonLabel text="Setting"/>
-            </View>
+        <View>
+            <ButtonLabel text="Setting" />
+        </View>
         </View>
       
        
-        </View>
+
     );
 }
 
@@ -91,7 +108,7 @@ const MenuScreen = (props) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        alignItems: 'center',
+        //alignItems: 'baseline',
         backgroundColor: '#E5E5E5', // or #E5E5E5
     },
     coverImage: {
@@ -115,19 +132,12 @@ const styles = StyleSheet.create({
         right: 0,
         bottom: 0,
     },
-    row: {
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        marginTop: 50,
-        height: 50,
-       
-    },
     buttonContainer: {
+        flex: 1,
         flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'space-between',
+        paddingVertical: 20,
         paddingHorizontal: 20,
+        marginTop: 30,
         flexWrap: 'wrap',
     },
     button: {
@@ -136,6 +146,7 @@ const styles = StyleSheet.create({
         height: 100,
         alignItems: 'center',
         justifyContent: 'center',
+        margin: 5,
     },
     text : {
         textAlign: 'center',
@@ -148,7 +159,12 @@ const styles = StyleSheet.create({
         fontSize: 14,
         textAlign: 'center',
         flexWrap: 'wrap',
+    },
+    imageIcon: {
+        width: 50,
+        height: 50,
     }
+
 
 })
 
