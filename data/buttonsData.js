8834f6ecec3d99ce8c3c592af5bd066db93f7a2e
Fitri <fitrirozi1@gmail.com>
2022-03-06 18:33:51 -0600

    reduced filenames

--- data/buttonsData.js
+++ data/buttonsData.js
@@ -14,58 +14,70 @@ export const buttonsData = [
     {
         label: "Student Life",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <Ionicons name="people" size={50} />,
+        Image: <Ionicons name="people" size={50} color="#003082" />,
     },
     {
         label: "Dining",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <MaterialIcons name="local-dining" size={50} />,
+        Image: <MaterialIcons name="local-dining" size={50} color="#003082" />,
     },
     {
         label: "Campus Cafe",
         link: "https://tab-web.scansoftware.com/cafeweb/login",
-        Image: <Ionicons name="cafe" size={50} />,
+        Image: <Ionicons name="cafe" size={50} color="#003082" />,
     },
     {
         label: "Calendar",
         link: "https://tabor.edu/calendar/",
-        Image: <FontAwesome5 name="calendar" size={50} />,
+        Image: <FontAwesome5 name="calendar" size={50} color="#003082" />,
     },
     {
         label: "Jayshop",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <MaterialCommunityIcons name="shopping-outline" size={50} />,
+        Image: (
+            <MaterialCommunityIcons
+                name="shopping-outline"
+                size={50}
+                color="#003082"
+            />
+        ),
     },
     {
         label: "Maps",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <FontAwesome5 name="map-marked-alt" size={50} />,
+        Image: <FontAwesome5 name="map-marked-alt" size={50} color="#003082" />,
     },
     {
         label: "Sports",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <Ionicons name="american-football" size={50} />,
+        Image: <Ionicons name="american-football" size={50} color="#003082" />,
     },
     {
         label: "Helpful Hours",
         link: "https://tabor.instructure.com/login/ldap",
         Image: (
-            <MaterialCommunityIcons name="account-clock-outline" size={50} />
+            <MaterialCommunityIcons
+                name="account-clock-outline"
+                size={50}
+                color="#003082"
+            />
         ),
     },
     {
         label: "News",
         link: "https://tabor.edu/news",
-        Image: <FontAwesome name="newspaper-o" size={50} />,
+        Image: <FontAwesome name="newspaper-o" size={50} color="#003082" />,
     },
     {
         label: "Notifications",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <Ionicons name="notifications-outline" size={40} />,
+        Image: (
+            <Ionicons name="notifications-outline" size={40} color="#003082" />
+        ),
     },
     {
         label: "Library",
         link: "https://taborcollege.libguides.com/library",
-        Image: <Ionicons name="library" size={40} />,
+        Image: <Ionicons name="library" size={40} color="#003082" />,
     },
 ];
