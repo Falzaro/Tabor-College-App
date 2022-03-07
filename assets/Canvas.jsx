360fbf9fbc793a53ea938d91810380e8708c62d6
Fitri <fitrirozi1@gmail.com>
2022-03-06 18:28:44 -0600

    added canvas

---
+++ assets/Canvas.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+
+const Canvas = (props) => (
+    <Svg
+        width={36}
+        height={36}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+        style={{ marginBottom: 7 }}
+    >
+        <Path
+            d="M6.333 1v32M27.666 1v32M1 6.333h32M1 27.667h32"
+            stroke="#000"
+            strokeWidth={3}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            color=""
+        />
+    </Svg>
+);
+
+export default Canvas;
--- data/buttonsData.js
+++ data/buttonsData.js
@@ -3,80 +3,69 @@ import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import Canvas from "../assets/Canvas";
 
 export const buttonsData = [
     {
         label: "Canvas",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: null,
+        Image: <Canvas />,
     },
     {
         label: "Student Life",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <Ionicons name="people" size={50} color="#003082" />,
+        Image: <Ionicons name="people" size={50} />,
     },
     {
         label: "Dining",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <MaterialIcons name="local-dining" size={50} color="#003082" />,
+        Image: <MaterialIcons name="local-dining" size={50} />,
     },
     {
         label: "Campus Cafe",
         link: "https://tab-web.scansoftware.com/cafeweb/login",
-        Image: <Ionicons name="cafe" size={50} color="#003082" />,
+        Image: <Ionicons name="cafe" size={50} />,
     },
     {
         label: "Calendar",
         link: "https://tabor.edu/calendar/",
-        Image: <FontAwesome5 name="calendar" size={50} color="#003082" />,
+        Image: <FontAwesome5 name="calendar" size={50} />,
     },
     {
         label: "Jayshop",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: (
-            <MaterialCommunityIcons
-                name="shopping-outline"
-                size={50}
-                color="#003082"
-            />
-        ),
+        Image: <MaterialCommunityIcons name="shopping-outline" size={50} />,
     },
     {
         label: "Maps",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <FontAwesome5 name="map-marked-alt" size={50} color="#003082" />,
+        Image: <FontAwesome5 name="map-marked-alt" size={50} />,
     },
     {
         label: "Sports",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: <Ionicons name="american-football" size={50} color="#003082" />,
+        Image: <Ionicons name="american-football" size={50} />,
     },
     {
         label: "Helpful Hours",
         link: "https://tabor.instructure.com/login/ldap",
         Image: (
-            <MaterialCommunityIcons
-                name="account-clock-outline"
-                size={50}
-                color="#003082"
-            />
+            <MaterialCommunityIcons name="account-clock-outline" size={50} />
         ),
     },
     {
         label: "News",
         link: "https://tabor.edu/news",
-        Image: <FontAwesome name="newspaper-o" size={50} color="#003082" />,
+        Image: <FontAwesome name="newspaper-o" size={50} />,
     },
     {
         label: "Notifications",
         link: "https://tabor.instructure.com/login/ldap",
-        Image: (
-            <Ionicons name="notifications-outline" size={40} color="#003082" />
-        ),
+        Image: <Ionicons name="notifications-outline" size={40} />,
     },
     {
         label: "Library",
         link: "https://taborcollege.libguides.com/library",
-        Image: <Ionicons name="library" size={40} color="#003082" />,
+        Image: <Ionicons name="library" size={40} />,
     },
 ];
