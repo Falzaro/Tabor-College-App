bf1aef73f52f854eda848ea3ba0478055064bb10
Fitri <fitrirozi1@gmail.com>
2022-04-07 11:52:09 -0500

    removed news and changed sports to athletics

--- screens/BottomTabs/Home.jsx
+++ screens/BottomTabs/Home.jsx
@@ -6,9 +6,8 @@ import { View, StyleSheet } from "react-native";
 // Relative Imports
 import TaborCollege from "../TaborCollege";
 import CafeMenu from "../CafeMenu";
-import Sports from "../Sports";
+import Athletics from "../Athletics";
 import HelpfulHours from "../HelpfulHours";
-import News from "../News";
 import Jayshop from "../Jayshop";
 import Library from "../Library";
 import StudentLife from "../StudentLife";
@@ -32,9 +31,8 @@ const Home = () => {
                 {/* Render the screen that the user is on */}
                 <Stack.Screen name="Tabor College" component={TaborCollege} />
                 <Stack.Screen name="Cafe Menu" component={CafeMenu} />
-                <Stack.Screen name="Sports" component={Sports} />
+                <Stack.Screen name="Athletics" component={Athletics} />
                 <Stack.Screen name="Helpful Hours" component={HelpfulHours} />
-                <Stack.Screen name="News" component={News} />
                 <Stack.Screen name="Jayshop" component={Jayshop} />
                 <Stack.Screen name="Library" component={Library} />
                 <Stack.Screen name="Student Life" component={StudentLife} />
