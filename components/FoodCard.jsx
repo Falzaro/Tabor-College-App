c0661ca37d4c1d7715e968abb27a54a4d73593b2
Fitri <fitrirozi1@gmail.com>
2022-04-09 14:04:36 -0500

    added separate images for different food titles

--- components/FoodCard.jsx
+++ components/FoodCard.jsx
@@ -3,18 +3,35 @@ import React from "react";
 import { View, StyleSheet, FlatList, ImageBackground } from "react-native";
 import { Card, Title, Caption } from "react-native-paper";
 
-// Relative Imports
-
 function FoodCard({ section }) {
-    const breakfastImg = require("../assets/cafe_menu/breakfast.jpg");
+    // const breakfastImg = require("../assets/cafe_menu/breakfast.jpg");
+    // const dinnerRotisserieImg = require("../assets/cafe_menu/dinner_rotisserie.jpg");
+    // const breakfastImg = require("../assets/cafe_menu/breakfast.jpg");
+
+    const getImage = (title) => {
+        switch (title) {
+            case "Breakfast":
+                return require("../assets/cafe_menu/breakfast.jpg");
+            case "Lunch":
+                return require("../assets/cafe_menu/lunch.jpg");
+            case "Lunch Rotisserie":
+                return require("../assets/cafe_menu/lunch_rotisserie.jpg");
+            case "Dinner":
+                return require("../assets/cafe_menu/dinner.jpg");
+            case "Dinner Rotisserie":
+                return require("../assets/cafe_menu/dinner_rotisserie.jpg");
+            default:
+                return require("../assets/cafe_menu/lunch.jpg");
+        }
+    };
 
     return (
         <Card style={styles.card}>
-            {/* Create custom card cover */}
             <ImageBackground
-                source={breakfastImg}
+                source={getImage(section.title)}
                 style={styles.cardCover}
                 imageStyle={{ borderRadius: 4 }}
+                // resize="center"
             />
             <View style={styles.cardContent}>
                 <View style={styles.titleWrapper}>
