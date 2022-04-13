4e63fac18fffce48d1aa21681434358d27a65b92
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2022-04-12 23:42:12 -0500

    grab infomation

---
+++ components/helpful_hours/DropDownTab.jsx
@@ -0,0 +1,100 @@
+import React, { useState} from 'react';
+import { View, FlatList, Text, Linking, StyleSheet} from 'react-native';
+import { Card, List, Title, Subheading } from 'react-native-paper';
+
+const DropDownTab = ({section}) => {
+
+    // useState for List Accordions
+    const [expanded, setExpanded] = useState(false); // set this to false, don't want to open automatically
+    const handlePress = () => setExpanded(!expanded);
+
+    return (
+        <View style = {styles.contentContainer}>
+            <List.Accordion
+                style = {styles.accordion}
+                title = {section.title}
+                theme ={{ colors: 
+                    {primary: "#003082" , 
+                    overflow: "hidden", 
+                    animation: "scale", 
+                    font: 'medium'
+                    }
+                
+                }} // this changes the Text when press to blue
+                expanded = {expanded}
+                onPress={handlePress} 
+                >
+                <Card style = { styles.card}>
+                <Title style = {styles.title}>Hours:</Title>
+                <FlatList
+                    showsVerticalScrollIndicator = {false}
+                    listKey={(item, index) => `_key${index.toString()}`}
+                    keyExtractor={(item, index) => `_key${index.toString()}`}
+                    data={section.data}
+                    renderItem={({item}) => (
+                        <View style = {styles.items}>
+                            <Subheading style = {styles.text}>{item}</Subheading>
+                        </View>
+                    )}
+                />
+                    <Title style = {styles.title}>Contact Information:</Title>
+                    <Subheading 
+                        onPress = {() => Linking.openURL(`tel:${section.phone}`)}
+                        style = {styles.contact}> {section.phone}
+                    </Subheading>
+                    <Subheading 
+                        onPress = {() => Linking.openURL(`mailto:${section.email}`)}
+                        style = {styles.contact}> {section.email}
+                    </Subheading>
+                    <Subheading 
+                        onPress = {() => Linking.openURL(`mailto:${section.email1}`)}
+                        style = {styles.contact} > {section.email1}
+                    </Subheading>
+                    <Subheading 
+                        onPress = {() => Linking.openURL(`mailto:${section.email2}`)}
+                        style = {styles.contact}> {section.email2}
+                    </Subheading>
+                </Card>
+            </List.Accordion>
+        </View>
+    );
+      
+}
+
+const styles = StyleSheet.create({
+    contentContainer: {
+        flex: 1,
+        paddingTop: 5,
+        width: "100%",
+    },
+    accordion:{
+        backgroundColor: 'white',
+        borderRadius: 5,
+        overflow: 'hidden',
+        paddingLeft: 0,
+     },
+    card: {
+        marginTop: 5,
+        alignItems: 'center',
+        marginBottom: 5,
+    },
+    items: {
+        alignItems: 'center',
+        marginTop: 5,
+    },
+    contact: {
+        textAlign: 'center',
+        color: '#003082',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 17,
+        fontWeight: 'bold',
+        paddingTop: 5,
+    },
+    text: {
+        textAlign: 'center',
+    }
+})
+
+export default DropDownTab;
--- screens/HelpfulHours.jsx
+++ screens/HelpfulHours.jsx
@@ -1,7 +1,16 @@
-import { StyleSheet, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, FlatList, View, Text } from "react-native";
 import Main from "../components/Main";
 
+import { db } from "../firebase/config";
+import { getDocs, collection } from "firebase/firestore";
+
+import DropDownTab from "../components/helpful_hours/DropDownTab";
+
+
 function HelpfulHours({ route }) {
+    const [helpfulHoursTab, setHelpfulHoursTab] = useState([]);
+   
     const { name } = route;
     const helpfulHoursCover = require("../assets/coverImage/helpfulHours.jpg");
     const coverImage = {
@@ -9,19 +18,47 @@ function HelpfulHours({ route }) {
         darkness: "rgba(0, 0, 0, 0.12)",
         blurRadius: 0.5,
     };
+
+    const item = [];
+    useEffect(() => { 
+        getDocs(collection(db, "test data"))
+            .then((snapshot) =>{
+                const helpfulHours = snapshot.docs.map((doc) => doc.data());
+                setHelpfulHoursTab(helpfulHours);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+          
+        
+    }, []);
+
     return (
         <Main name={name} coverImage={coverImage}>
-            <View style={styles.center}>{/* No Content */}</View>
+            <View style={styles.center}>
+                <FlatList 
+                    showsVerticalScrollIndicator = {false}
+                    data={helpfulHoursTab}
+                    listKey={(item, index) => `_key${index.toString()}`}
+                    keyExtractor={(item, index) => `_key${index.toString()}`}
+                    renderItem={({ item: section }) => (
+                        <DropDownTab section={section} />
+                    )}
+                />
+            </View>
         </Main>
     );
 }
+
+    
 
 export default HelpfulHours;
 
 const styles = StyleSheet.create({
     center: {
         flex: 1,
+        paddingHorizontal: 18,
         justifyContent: "center",
-        alignItems: "center",
+        //alignItems: "center",
     },
 });
