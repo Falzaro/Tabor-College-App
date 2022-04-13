448f8a7439d5626cb2f3e9554a22e5b4564e97bf
Fitri <fitrirozi1@gmail.com>
2022-04-13 00:39:16 -0500

    installed react native maps package

--- screens/Maps.jsx
+++ screens/Maps.jsx
@@ -1,4 +1,5 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 import Main from "../components/Main";
 
 function Maps({ route }) {
@@ -11,7 +12,18 @@ function Maps({ route }) {
     };
     return (
         <Main name={name} coverImage={coverImage}>
-            <View style={styles.center}>{/* No Content */}</View>
+            <View style={styles.center}>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 38.34851,
+                        longitude: -97.20017,
+                        latitudeDelta: 0.0012,
+                        longitudeDelta: 0.0011,
+                    }}
+                    provider="google"
+                />
+            </View>
         </Main>
     );
 }
@@ -21,7 +33,11 @@ export default Maps;
 const styles = StyleSheet.create({
     center: {
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
+    },
+    map: {
+        height: 280,
+        width: "100%",
+        marginVertical: 10,
     },
 });
