5716cedeefd3baa23758ea195e908504b9e89d4d
Fitri <fitrirozi1@gmail.com>
2022-04-16 15:38:53 -0500

    fixed text error and handled empty contact info

--- components/helpful_hours/ContactInformation.jsx
+++ components/helpful_hours/ContactInformation.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import { Linking, StyleSheet } from "react-native";
 import { Card, Title, Subheading } from "react-native-paper";
 
 const ContactInformation = ({ contactInfo }) => {
+    const [isContactInfoAvailable, setIsContactInfoAvailable] = useState(true);
+
+    useEffect(() => {
+        // Check if all contactInfo next values are empty
+        if (Object.values(contactInfo).every((value) => value === "")) {
+            setIsContactInfoAvailable(false);
+        }
+    }, []);
+
     return (
         <Card style={styles.card}>
             <Title style={styles.title}>Contact Information</Title>
             {/* Phone number */}
-            {contactInfo.phone && (
+            {Boolean(contactInfo.phone) && (
                 <Subheading
                     onPress={() => Linking.openURL(`tel:${contactInfo.phone}`)}
                     style={styles.contact}
@@ -15,18 +25,18 @@ const ContactInformation = ({ contactInfo }) => {
                 </Subheading>
             )}
             {/* Email 1 */}
-            {contactInfo.email && (
+            {Boolean(contactInfo.email1) && (
                 <Subheading
                     onPress={() =>
-                        Linking.openURL(`mailto:${contactInfo.email}`)
+                        Linking.openURL(`mailto:${contactInfo.email1}`)
                     }
                     style={styles.contact}
                 >
-                    {contactInfo.email}
+                    {contactInfo.email1}
                 </Subheading>
             )}
             {/* Email 2 */}
-            {contactInfo.email2 && (
+            {Boolean(contactInfo.email2) && (
                 <Subheading
                     onPress={() =>
                         Linking.openURL(`mailto:${contactInfo.email2}`)
@@ -45,7 +55,7 @@ const ContactInformation = ({ contactInfo }) => {
                     {contactInfo.url}
                 </Subheading>
             )}
-            {!contactInfo && (
+            {!isContactInfoAvailable && (
                 <Subheading style={{ textAlign: "center" }}>
                     No contact information available
                 </Subheading>
