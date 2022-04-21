20362a8c599211a683a839a5f50cc3c3bd030d13
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2022-04-20 19:40:31 -0500

    Remove unnessary and update Lib

--- screens/Library.jsx
+++ screens/Library.jsx
@@ -1,11 +1,13 @@
-import { StyleSheet, View, Text, TouchableOpacity, Linking, FlatList, SafeAreaView } from "react-native";
+import React, { useState, useEffect} from 'react';
+import { StyleSheet, View, Text, Linking, FlatList, SafeAreaView } from "react-native";
 import Main from "../components/Main";
-import { Card, Title, Subheading} from 'react-native-paper';
-import { EvilIcons } from '@expo/vector-icons';
+import { Card, Title, Subheading, Paragraph, Button } from 'react-native-paper';
 
-import LibraryData from '../components/library_screen/LibraryData';
+// import firebase 
 
-const libraryLink = "https://taborcollege.libguides.com/library"
+import {db} from '../firebase/config';
+import { doc, getDoc } from 'firebase/firestore';
+
 // set Locations
 const latitude = "38.34882";
 const longitude = "-97.201";
@@ -18,6 +20,25 @@ const url = Platform.select({
 });
 
 function Library({ route }) {
+    const [libraryData, setLibraryData] = useState([]);
+
+    useEffect(() => {
+        // get Jay Shop data from Firestore
+        const docRef = doc(db, "helpful hours example", "library hours");
+        getDoc(docRef)
+            .then((doc) => {
+                const data = doc.data();
+                setLibraryData(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, []);
+
+    // Deconstruct Just to call the open hours
+    const {"Contact information": contactInfo, "open hours": openHours } = 
+        libraryData;
+
     const { name } = route;
     const libraryCover = require("../assets/coverImage/library.jpg");
     const coverImage = {
@@ -28,22 +49,49 @@ function Library({ route }) {
     return (
        
         <Main name={name} coverImage={coverImage}>
-            <View style={styles.center}>
-                <Card style = {styles.card}>
-                    <Title style = {styles.title}>Welcome to Tabor Library</Title>
-                    <View>
-                        <TouchableOpacity style ={styles.visitUs} onPress = {() => Linking.openURL(libraryLink)}>
-                            <Subheading style = {{color: "#003082", paddingBottom: 5}}>Visit Website!</Subheading>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity style={styles.buttonPress} onPress = {() => Linking.openURL(url)}>
-                            <EvilIcons style ={styles.icons} size={30} color = "#003082" name ="location"/> 
-                            <Subheading style = {styles.textLocation}>Location</Subheading>
-                        </TouchableOpacity>
-                    </View>
+            <View  style = {styles.container}>
+                <Card style = {styles.card} >
+                <Title style ={styles.title}>{libraryData.title}</Title>
+                    <Card.Content>
+                    <Paragraph style = {styles.memo}>{libraryData.memo}</Paragraph>
+                    <Card.Cover style = {styles.image} source = {{uri: libraryData.image}}/>
+                        <FlatList
+                            style={{ marginBottom: 5, }}
+                            showsVerticalScrollIndicator={false}
+                            listKey={(_, index) => `_key${index}`}
+                            keyExtractor={(_, index) => `_key${index}`}
+                            data={openHours}
+                            renderItem={({ item }) => (
+                                <View style = {styles.alignSideBySide}>
+                                    {/* Show days */}
+                                    <Subheading styles ={styles.days} >{item.days}</Subheading>
+                                    {/* Map out the hours */}
+                                    <FlatList
+                                        showsVerticalScrollIndicator={false}
+                                        listKey={(_, index) => `_key${index}`}
+                                        keyExtractor={(_, index) => `_key${index}`}
+                                        data={item.hours}
+                                        renderItem={({ item: hours }) => (
+                                            <Subheading styles = {styles.hours} >{hours}</Subheading>
+                                        )}
+                                    />
+                                    
+                                </View>
+                            )}
+                        />
+                        </Card.Content>
+                        {/* using Card.Actions "Button" components. Please see doc for more information */}
+                        <Card.Actions style = {{justifyContent: 'space-between'}}>
+                            <Button mode="contained" icon = "web" color ="#003082" title ="Visit Tabor Library"
+                                onPress={() => Linking.openURL(contactInfo.url)}>
+                                Visit Tabor Library
+                            </Button>
+                            <Button mode="contained"  icon="map-marker-radius-outline" color= "#003082" title ="Location"  onPress = {() => Linking.openURL(url)}>
+                                Location 
+                            </Button>
+                    </Card.Actions>
                 </Card>
-                <LibraryData />
-            </View>
+        </View>
         </Main>
       
     );
@@ -52,35 +100,47 @@ function Library({ route }) {
 export default Library;
 
 const styles = StyleSheet.create({
-    center: {
+    container:{
         flex: 1,
         paddingHorizontal: 18,
-        marginTop: 10, 
-      //  paddingBottom: 10
+        paddingTop: 10,
     },
-    buttonPress:{
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginBottom: 5,
+    card:{
+       borderWidth: 1,
     },
-    icons:{
+    hours:{  // align the hours to the -> right 
         alignSelf: 'flex-end',
-        
     },
-    textLocation:{
-        color: "#003082",
-        //fontSize: 18,
+    days:{ // align to the start -> left
+        alignSelf: 'flex-start',
+    },
+    alignSideBySide:{ // this align the Days and Hours next to each other but space-between 
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 5,
+        marginTop: 10,
+    },
+    title:{
+        textAlign: "center",
+        fontSize: 18,
+        fontWeight: "bold",
+        paddingTop: 5,
+    },
+    memo:{
+        textAlign: "center",
         fontStyle: "italic",
+        marginTop: 5,
+        marginBottom: 5,
+        color: "#003082",
+        fontSize: 15,
     },
-    title: {
-        marginLeft: 10,
-        textAlign: 'center',
+    image: {
+        width: "100%",
+        height:"45%",
+        //marginBottom: 5
     },
-    card: {
-        //borderColor: '#003082',
-       // borderWidth: 1
-    },
-    visitUs:{
-        alignItems: 'center',
+    hyperLink: {
+        color: "#003082"
     }
+   
 });
