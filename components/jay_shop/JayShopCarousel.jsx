db0c666756e26bcea0a386a7be4e4ec6e0f9d510
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2022-04-24 00:54:05 -0500

    Fix height image

---
+++ components/jay_shop/JayShopCarousel.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react';
+import { Image, View, Linking,  StyleSheet, ImageBackground,  TouchableOpacity, Button, Dimensions } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import { Card, Title, Subheading } from 'react-native-paper';
+
+// Relative Imports
+import { db } from '../../firebase/config';
+import { doc, getDoc } from "firebase/firestore";
+
+const SLIDER_WIDTH = Dimensions.get('window').width;
+const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.5);
+const ITEM_HEIGHT = Math.round(ITEM_WIDTH * 3 / 4);
+
+const image = {uri: "https://tabor.edu/wp-content/plugins/phastpress/phast.php?service=images&src=https%3A%2F%2Ftabor.edu%2Fwp-content%2Fthemes%2Ftabor-theme%2Fassets%2Fimg%2Fhome-footer.jpg&cacheMarker=1554954396-93644&token=ac3057f43d6d5d49"};
+
+const JayShopCarousel = () => {
+    const [getImage, setImage] = useState();
+
+    useEffect(() => {
+        // Get the  from firebase version 9
+        const docRef = doc(db, "products", "testing data3");
+        getDoc(docRef)
+           
+            .then((doc) => {
+                setImage(doc.data().sections);
+                
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    return (
+
+        <View style = {styles.container}>
+        
+            <Title style ={styles.title}>Available Products:</Title>
+
+            <Carousel 
+                layout = {'default'}
+                data={getImage}
+                decelerationRate = 'fast'
+                useScrollView={true}
+                sliderWidth={SLIDER_WIDTH}
+                itemWidth={ITEM_WIDTH}
+                renderItem={({ item: section }) => (
+                    <View style ={styles.card}>  
+                    <Image style = {styles.imageLayout} source = {{uri: section.image}} />
+                    <Subheading style={styles.price}>{section.price}</Subheading>
+                    <Subheading style ={styles.name}>{section.name}</Subheading>
+                    </View>
+                   
+                )}
+            />
+            <TouchableOpacity style = {styles.button}   onPress={() => Linking.openURL('https://tabor.edu/shop/')}>
+                <Subheading style = {styles.buttonText}>View More</Subheading>
+            </TouchableOpacity>
+       
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: 'transparent',
+        marginTop: 5,
+        overflow: 'hidden',
+        alignItems: "center",
+        borderRadius: 5,
+    },
+    imageLayout:{
+        width:200,
+        height: 200,
+        borderRadius: 5,
+        justifyContent: 'center',
+        marginTop: 5,
+        borderColor: "#f7d117",
+        borderWidth: 1,
+
+    },
+    title:{
+        textAlign: "center",
+        fontSize: 18,
+        fontWeight: "bold",
+        paddingTop: 5,
+        paddingHorizontal: 10,
+    },
+    price:{
+        alignItems: "center",
+        paddingHorizontal: 10,
+        color: 'white',
+    },
+    card:{
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 5,
+        backgroundColor: "#003082",
+        borderColor: "#f7d117",
+        borderWidth: 1,
+      
+    },
+    button: {
+        alignItems: "center",
+        marginBottom: 5,
+        
+    },
+    buttonText:{
+        color: '#003082',
+        fontSize: 20,
+    },
+   name:{
+        textAlign: "center",
+        color: 'white',
+        paddingHorizontal: 10,
+   },
+  
+   
+   
+
+})
+export default JayShopCarousel;
---
+++ components/jay_shop/JayShopHours.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect} from 'react';
+import { StyleSheet, View, FlatList, Linking, Text } from "react-native";
+
+import { Card, List, Title, Subheading, } from 'react-native-paper';
+// import firebase 
+
+import {db} from '../../firebase/config';
+import { doc, getDoc } from 'firebase/firestore';
+
+const JayShopHours = () => {
+    const [jayShop, setJayShop] = useState([]);
+
+    useEffect(() => {
+        // get Jay Shop data from Firestore
+        const docRef = doc(db, "helpful hours example", "jay shop hours");
+        getDoc(docRef)
+            .then((doc) => {
+                const data = doc.data();
+                setJayShop(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, []);
+
+    const { "Contact information": contactInfo, "open hours": openHours } =
+    jayShop;
+
+    return (
+        <View  style = {styles.container}>
+                <Card style = {styles.card} >
+                    <Title style ={styles.title}>{jayShop.title}</Title>
+                    <Text style = {styles.location}>{jayShop.location}</Text>
+                    <FlatList
+                        style={{ marginBottom: 5, }}
+                        showsVerticalScrollIndicator={false}
+                        listKey={(_, index) => `_key${index}`}
+                        keyExtractor={(_, index) => `_key${index}`}
+                        data={openHours}
+                        renderItem={({ item }) => (
+                            <View style = {styles.alignSideBySide}>
+                                {/* Show days */}
+                                <Subheading styles ={styles.days} >{item.days}</Subheading>
+                                {/* Map out the hours */}
+                                <FlatList
+                                    showsVerticalScrollIndicator={false}
+                                    listKey={(_, index) => `_key${index}`}
+                                    keyExtractor={(_, index) => `_key${index}`}
+                                    data={item.hours}
+                                    renderItem={({ item: hours }) => (
+                                        <Subheading styles = {styles.hours} >{hours}</Subheading>
+                                    )}
+                                />
+                            </View>
+                        )}
+                    />
+            </Card>
+            </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        marginTop: 5,
+       
+       paddingHorizontal: 18,
+    },
+    card:{
+        paddingHorizontal: 10, 
+    },
+    hours:{  // align the hours to the -> right 
+        alignSelf: 'flex-end',
+    },
+    days:{ // align to the start -> left
+        alignSelf: 'flex-start',
+
+    },
+    alignSideBySide:{ // this align the Days and Hours next to each other but space-between 
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    title:{
+        textAlign: "center",
+        fontSize: 18,
+        fontWeight: "bold",
+        paddingTop: 5,
+    },
+    location:{
+        textAlign: "center",
+        fontStyle: "italic",
+        marginBottom: 7,
+    },
+
+});
+
+export default JayShopHours;
