9435a15ff752cf5981eee5b3cead9f6b9ae98323
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2022-04-27 00:17:09 -0500

    add jsx

---
+++ components/about_us/About_us.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const About_us = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+})
+export default About_us;
