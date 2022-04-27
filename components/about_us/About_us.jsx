39b91bc99415d5574c7f0fbfbadaee87498d53f5
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2022-04-27 17:35:14 -0500

    commit

--- components/about_us/About_us.jsx
+++ components/about_us/About_us.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View , Text} from 'react-native';
+import { Title, Caption, Headline} from 'react-native-paper';
 
 const About_us = () => {
     return (
-        <View>
-            
+        <View style = {styles.container}>
+            <Headline>About Us!</Headline>
+            <Caption style = {{fontSize: 20, color: 'rgba(255, 205, 0, 1)', marginTop: 10}}>Wichita State University</Caption>
+            <Caption style = {{fontSize: 18, color: '#27251F', }}>WuShock Go</Caption>
+            <Caption style = {{fontSize: 15, marginTop: 5 }}>Team Members:</Caption>
+            <Text style = {{ marginTop: 5 }}>Names</Text>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white'
+    }
 })
 export default About_us;
