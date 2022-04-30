26780e3ff9dc9c5ba9e2ed7ef26da5036480cb78
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2022-04-30 08:00:12 -0500

    layout changes

--- screens/BottomTabs/Settings.jsx
+++ screens/BottomTabs/Settings.jsx
@@ -1,19 +1,20 @@
 import { StyleSheet, View, Text } from "react-native";
+import { Title } from "react-native-paper";
 
-import About_us from '../../components/about_us/About_us';
+import About_us from "../../components/about_us/About_us";
 
 function Settings() {
-    return (
-        <View style={styles.center}>
-            <About_us />
-        </View>
-    );
+  return (
+    <View style={styles.center}>
+      <About_us />
+    </View>
+  );
 }
 
 export default Settings;
 
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-    },
+  center: {
+    flex: 1,
+  },
 });
