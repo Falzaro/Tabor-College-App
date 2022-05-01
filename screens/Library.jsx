5f050450577c3b865e71eea316c7c39b77ec1e04
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2022-05-01 15:34:31 -0500

    Fix duplicate index key

--- screens/Library.jsx
+++ screens/Library.jsx
@@ -4,27 +4,30 @@ import Main from "../components/Main";
 import LibraryHours from "../components/library_screen/Library_Hours";
 
 function Library({ route }) {
-  const { name } = route;
-  const libraryCover = require("../assets/coverImage/library.jpg");
-  const coverImage = {
-    source: libraryCover,
-    darkness: "rgba(0, 0, 0, 0.12)",
-    blurRadius: 1,
-  };
-  return (
-    <Main name={name} coverImage={coverImage}>
-      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-        <LibraryHours />
-      </ScrollView>
-    </Main>
-  );
+    const { name } = route;
+    const libraryCover = require("../assets/coverImage/library.jpg");
+    const coverImage = {
+        source: libraryCover,
+        darkness: "rgba(0, 0, 0, 0.12)",
+        blurRadius: 1,
+    };
+    return (
+        <Main name={name} coverImage={coverImage}>
+            <ScrollView
+                style={styles.container}
+                showsVerticalScrollIndicator={false}
+            >
+                <LibraryHours />
+            </ScrollView>
+        </Main>
+    );
 }
 
 export default Library;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 18,
-  },
+    container: {
+        flex: 1,
+        paddingHorizontal: 18,
+    },
 });
