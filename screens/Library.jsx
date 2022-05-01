82f1b0ee2408c5c15374bf766a0c7747488bbf9d
ttran0012 <60831602+ttran0012@users.noreply.github.com>
2022-04-30 21:37:42 -0500

    cc

--- screens/Library.jsx
+++ screens/Library.jsx
@@ -1,27 +1,29 @@
 import { StyleSheet, View, Text } from "react-native";
 import Main from "../components/Main";
-
+import { WebView } from "react-native-webview";
 function Library({ route }) {
-    const { name } = route;
-    const libraryCover = require("../assets/coverImage/library.jpg");
-    const coverImage = {
-        source: libraryCover,
-        darkness: "rgba(0, 0, 0, 0.12)",
-        blurRadius: 1,
-    };
-    return (
-        <Main name={name} coverImage={coverImage}>
-            <View style={styles.center}>{/* No Content */}</View>
-        </Main>
-    );
+  const { name } = route;
+  const libraryCover = require("../assets/coverImage/library.jpg");
+  const coverImage = {
+    source: libraryCover,
+    darkness: "rgba(0, 0, 0, 0.12)",
+    blurRadius: 1,
+  };
+  return (
+    <Main name={name} coverImage={coverImage}>
+      <View style={styles.center}>
+          
+      </View>
+    </Main>
+  );
 }
 
 export default Library;
 
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
